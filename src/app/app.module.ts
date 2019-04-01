66314170497e7bb10778d43e3ffb58a00718b2d0
Vladislav <olsovka@gmail.com>
2019-04-01 14:42:28 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {Routes, Router, RouterModule} from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { MynewcomponentComponent } from './mynewcomponent/mynewcomponent.component';
+import { DateComponent } from './date/date.component';
+import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { PriceComponent } from './price/price.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {NewServiceService} from './new-service.service';
+import {HttpClientModule} from '@angular/common/http';
+
+// определяем паршруты
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'price', component: PriceComponent},
+  {path: '**', component: NotFoundComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MynewcomponentComponent,
+    DateComponent,
+    HomeComponent,
+    AboutComponent,
+    NewsComponent,
+    PriceComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule,
+      RouterModule.forRoot(appRoutes),
+      HttpClientModule
   ],
-  providers: [],
+  providers: [NewServiceService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
